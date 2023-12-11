b96ae00e3507692c15e72bc68a31b935b9af3ba9
bandriessen <bandriessen@student.tudelft.nl>
2023-12-11 15:39:30 +0100

    test first error plots

---
+++ trajectory_generation/Code/error_quantification.py
@@ -0,0 +1,54 @@
+# Load standard modules
+import numpy as np
+
+import matplotlib
+from matplotlib import pyplot as plt
+
+# Load tudatpy modules
+from tudatpy.kernel.astro import frame_conversion
+
+import pandas as pd
+import os
+
+#-----------------------Directories-----------------------#
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+
+# Create folder for files if needed
+file_path = os.path.join(parent_dir, "Files").replace("\\.", ".")
+if os.path.exists(file_path):
+    pass
+else:
+    os.makedirs(file_path)
+
+figures_path = os.path.join(parent_dir, "Figures").replace("\\.", ".")
+if os.path.exists(figures_path):
+    pass
+else:
+    os.makedirs(figures_path)
+
+
+
+#---------------------------Plotting---------------------------#
+
+states = np.genfromtxt(os.path.join(file_path,"states.txt").replace("\\.", "."), delimiter=',')
+states_GPS = np.genfromtxt(os.path.join(file_path,"states_GPS.txt").replace("\\.", "."), delimiter=',')
+
+fig, ax = plt.subplots()
+ax.set_title("position components error over time")
+ax.scatter(states[:,0], states_GPS[:,1]-states[:,1], s=1,label='x')
+ax.scatter(states[:,0], states_GPS[:,2]-states[:,2], s=1,label='y')
+ax.scatter(states[:,0], states_GPS[:,3]-states[:,3], s=1,label='z')
+ax.set_xlabel('time [s]')
+ax.set_ylabel('position component error [m]')
+ax.grid()
+ax.legend()
+
+output_path = os.path.join(figures_path,"states_position_error.pdf").replace("\\.", ".")
+fig.savefig(output_path, bbox_inches='tight')
+
+
+
+
+plt.show()
