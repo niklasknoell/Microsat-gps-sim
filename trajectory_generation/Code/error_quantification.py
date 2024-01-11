9c6d7f06c14d91502d0b7c5081177e0a9a64a61f
bandriessen <bandriessen@student.tudelft.nl>
2024-01-11 19:41:34 +0100

    add RSW error plots

--- trajectory_generation/Code/error_quantification.py
+++ trajectory_generation/Code/error_quantification.py
@@ -68,18 +68,50 @@ fig.savefig(output_path, bbox_inches='tight')
 
 
 
+RSW_error = np.genfromtxt(os.path.join(file_path,"RSW_error.txt").replace("\\.", "."), delimiter=',')
+
+fig, ax = plt.subplots()
+# ax.set_title("position components error over time")
+ax.scatter(states[:,0], RSW_error[:,0], s=1,label='R')
+ax.scatter(states[:,0], RSW_error[:,1], s=1,label='S')
+ax.scatter(states[:,0], RSW_error[:,2], s=1,label='W')
+ax.set_xlabel('time [s]')
+ax.set_ylabel('Error in R, S, W directions [m]')
+ax.grid()
+ax.legend()
+
+
+output_path = os.path.join(figures_path,"RSW_position_error.pdf").replace("\\.", ".")
+fig.savefig(output_path, bbox_inches='tight')
+
+
+fig, ax = plt.subplots()
+# ax.set_title("position components error over time")
+ax.scatter(states[:,0], RSW_error[:,3], s=1,label='R')
+ax.scatter(states[:,0], RSW_error[:,4], s=1,label='S')
+ax.scatter(states[:,0], RSW_error[:,5], s=1,label='W')
+ax.set_xlabel('time [s]')
+ax.set_ylabel('Error in R, S, W directions [m/s]')
+ax.grid()
+ax.legend()
+
+output_path = os.path.join(figures_path,"RSW_velocity_error.pdf").replace("\\.", ".")
+fig.savefig(output_path, bbox_inches='tight')
+
+
+
 # RSW = np.genfromtxt(os.path.join(file_path,"RSW.txt").replace("\\.", "."), delimiter=',')
 # RSW_GPS = np.genfromtxt(os.path.join(file_path,"RSW_GPS.txt").replace("\\.", "."), delimiter=',')
-#
-# fig, ax = plt.subplots()
-# # ax.set_title("position components error over time")
-# ax.scatter(states[:,0], RSW_GPS[:,0]-RSW[:,0], s=1,label='$\Delta$R')
-# ax.scatter(states[:,0], RSW_GPS[:,0]-RSW[:,0], s=1,label='$\Delta$S')
-# ax.scatter(states[:,0], RSW_GPS[:,0]-RSW[:,0], s=1,label='$\Delta$W')
-# ax.set_xlabel('time [s]')
-# ax.set_ylabel('|position component error| [m]')
-# ax.grid()
-# ax.legend()
+# #
+# # fig, ax = plt.subplots()
+# # # ax.set_title("position components error over time")
+# # ax.scatter(states[:,0], RSW_GPS[:,0]-RSW[:,0], s=1,label='$\Delta$R')
+# # ax.scatter(states[:,0], RSW_GPS[:,0]-RSW[:,0], s=1,label='$\Delta$S')
+# # ax.scatter(states[:,0], RSW_GPS[:,0]-RSW[:,0], s=1,label='$\Delta$W')
+# # ax.set_xlabel('time [s]')
+# # ax.set_ylabel('|position component error| [m]')
+# # ax.grid()
+# # ax.legend()
 
 
 
