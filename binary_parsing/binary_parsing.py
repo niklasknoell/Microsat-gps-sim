ada1596b5b6ee61bb4020472e7f51d00912f1e14
MauritsKuipers <74401262+MauritsKuipers@users.noreply.github.com>
2024-01-26 16:36:59 +0100

    Fixed aan error in the binary parser

--- binary_parsing/binary_parsing.py
+++ binary_parsing/binary_parsing.py
@@ -1,6 +1,6 @@
 import os
 import json
-
+import numpy as np
 
 def translate_line(line):
     # This code makes the assumption that the binaty message is saved in the text file in Hex
@@ -8,105 +8,128 @@ def translate_line(line):
     # Sectioning the data in the message
     message_body = line[8:-6]
     translation = {}
+    hex_line = {"total": line, "message_body": message_body}
 
     # Message ID
-    message_id = message_body[0:1]
+    message_id = message_body[0:2]
     translation["Message_ID"] = message_id
+    hex_line["message_id"]= message_id
 
     # Fix Mode Interpretation
-    fix_mode = message_body[2:3]
+    fix_mode = message_body[2:4]
     fix_mode_meaning = translate_fix_mode_section(fix_mode)
     translation["Fix_Mode"] = fix_mode_meaning
+    hex_line["fix_mode"]= fix_mode
 
     # Number of SV in fix
-    SV_number = message_body[4:5]
+    SV_number = message_body[4:6]
     sv_decimal = translate_sv_number(SV_number)
     translation["SV_Number"] = sv_decimal
+    hex_line["SV_number"]= SV_number
 
     # GNSS week number
-    gnss_week = message_body[6:9]
+    gnss_week = message_body[6:10]
     gnss_week_decimal = translate_GNSS_week(gnss_week)
     translation["GNSS_Week"] = gnss_week_decimal
+    hex_line["gnss_week"]= gnss_week
 
     # TOW interpretation
-    TOW_hex = message_body[10:17]
+    TOW_hex = message_body[10:18]
     TOW_decimal = translate_TOW(TOW_hex)
     translation["TOW"] = TOW_decimal
+    hex_line["TOW_hex"]= TOW_hex
 
     # Latitude Translation
-    latitude_hex = message_body[18:25]
+    latitude_hex = message_body[18:26]
     latitude_decimal = hex_to_decimal_latitude(latitude_hex)
     translation["Latitude"] = latitude_decimal
+    hex_line["latitude_hex"]= latitude_hex
 
     # Longitude Translation
-    longitude_hex = message_body[26:33]
+    longitude_hex = message_body[26:34]
     longitude_decimal = hex_to_decimal_longitude(longitude_hex)
     translation["Longitude"] = longitude_decimal
+    hex_line["longitude_hex"]= longitude_hex
 
     # Ellipsoid Altitude Translation
-    ellipsoid_hex = message_body[34:41]
+    ellipsoid_hex = message_body[34:42]
     ellipsoid_decimal = hex_to_decimal_ellipsoid(ellipsoid_hex)
     translation["Ellipsoid_Altitude"] = ellipsoid_decimal
+    hex_line["ellipsoid_hex"]= ellipsoid_hex
 
     # Mean Sea Level Altitude
-    sea_altitude_hex = message_body[42:49]
+    sea_altitude_hex = message_body[42:50]
     sea_altitude_decimal = hex_to_decimal_sea_level_altitude(sea_altitude_hex)
     translation["Sea_Level_Altitude"] = sea_altitude_decimal
+    hex_line["sea_altitude_hex"]= sea_altitude_hex
 
     # GDOP Translation
-    GDOP_hex = message_body[50:53]
+    GDOP_hex = message_body[50:54]
     GDOP_decimal = hex_to_decimal_GDOP(GDOP_hex)
     translation["GDOP"] = GDOP_decimal
+    hex_line["GDOP_hex"]= GDOP_hex
 
     # PDOP Translation
-    PDOP_hex = message_body[54:57]
+    PDOP_hex = message_body[54:58]
     PDOP_decimal = hex_to_decimal_PDOP(PDOP_hex)
     translation["PDOP"] = PDOP_decimal
+    hex_line["PDOP_hex"]= PDOP_hex
 
     # HDOP Translation
-    HDOP_hex = message_body[58:61]
+    HDOP_hex = message_body[58:62]
     HDOP_decimal = hex_todecimal_HDOP(HDOP_hex)
     translation["HDOP"] = HDOP_decimal
+    hex_line["HDOP_hex"]= HDOP_hex
 
     # VDOP Translation
-    VDOP_hex = message_body[62:65]
+    VDOP_hex = message_body[62:66]
     VDOP_decimal = hex_to_decimal_VDOP(VDOP_hex)
     translation["VDOP"] = VDOP_decimal
+    hex_line["VDOP_hex"]= VDOP_hex
 
     # TDOP Translation
-    TDOP_hex = message_body[66:69]
+    TDOP_hex = message_body[66:70]
     TDOP_decimal = hex_to_decimal_TDOP(TDOP_hex)
     translation["TDOP"] = TDOP_decimal
+    hex_line["TDOP_hex"]= TDOP_hex
 
     # ECEF-X Translation
-    ECEFX_hex = message_body[70:77]
+    ECEFX_hex = message_body[70:78]
     ECEFX_decimal = hex_to_decimal_ECEFX(ECEFX_hex)
     translation["ECEFX"] = ECEFX_decimal
+    hex_line["ECEFX_hex"]= ECEFX_hex
 
     # ECEF-Y Translation
-    ECEFY_hex = message_body[78:85]
+    ECEFY_hex = message_body[78:86]
     ECEFY_decimal = hex_to_decimal_ECEFY(ECEFY_hex)
-    translation["ECEFX"] = ECEFY_decimal
+    translation["ECEFY"] = ECEFY_decimal
+    hex_line["ECEFY_hex"]= ECEFY_hex
 
     # ECEF-Z Translation
-    ECEFZ_hex = message_body[86:93]
+    ECEFZ_hex = message_body[86:94]
     ECEFZ_decimal = hex_to_decimal_ECEFZ(ECEFZ_hex)
-    translation["ECEFX"] = ECEFZ_decimal
+    translation["ECEFZ"] = ECEFZ_decimal
+    hex_line["ECEFZ_hex"]= ECEFZ_hex
 
     # ECEF-VX Translation
-    ECEFVX_hex = message_body[94:101]
+    ECEFVX_hex = message_body[94:102]
     ECEFVX_decimal = hex_to_decimal_ECEFVX(ECEFVX_hex)
     translation["ECEF-VX"] = ECEFVX_decimal
+    hex_line["ECEFVX_hex"]= ECEFVX_hex
 
     # ECEF-VY Translation
-    ECEFVY_hex = message_body[102:109]
+    ECEFVY_hex = message_body[102:110]
     ECEFVY_decimal = hex_to_decimal_ECEFVY(ECEFVY_hex)
     translation["ECEF-VY"] = ECEFVY_decimal
+    hex_line["ECEFVY_hex"]= ECEFVY_hex
 
     # ECEF-VZ Translation
-    ECEFVZ_hex = message_body[110:117]
+    ECEFVZ_hex = message_body[110:118]
     ECEFVZ_decimal = hex_to_decimal_ECEFVZ(ECEFVZ_hex)
     translation["ECEF-VZ"] = ECEFVZ_decimal
+    hex_line["ECEFVZ_hex"]= ECEFVZ_hex
+
+    print(hex_line)
 
     return translation
 
@@ -137,7 +160,7 @@ def translate_GNSS_week(gnss_week_hex):
 
 def translate_TOW(tow_hex):
     if tow_hex:
-        decimal_value = int(str(tow_hex), 16)
+        decimal_value = np.uint32(int(tow_hex, 16))
         return str(decimal_value)
     else:
         # Handle the case where tow_hex is empty (return a default value or raise an exception)
