b3034db6a99f0d9cc872fd9edd1d74f93fc13b25
MauritsKuipers <74401262+MauritsKuipers@users.noreply.github.com>
2024-01-29 13:02:27 +0100

    made a version of saving the binary output based on determining payload length and use that to save the output all together.

--- recording/BINARY_store.py
+++ recording/BINARY_store.py
@@ -14,7 +14,23 @@ logFile.parent.mkdir(parents=True, exist_ok=True)
 file = logFile.open("+ab", buffering=64)
 timeout = 80
 
-while True:
-    line = port.readline()
-    hex_line = line.hex()
-    file.write(hex_line.encode('utf-8') + b'\n')
+def saving_method1():
+    while True:
+        line = port.readline()
+        hex_line = line.hex()
+        file.write(hex_line.encode('utf-8') + b'\n')
+
+
+def saving_method():
+    while True:
+        start_message = port.read(4)
+        payload_length_hex = start_message[-2:].hex()
+        payload_length_decimal = int(payload_length_hex, 16)
+        message_payload_and_end = port.read(payload_length_decimal + 3)
+        total_message = str(start_message) + str(message_payload_and_end)
+        total_message_hex = total_message.hex()
+        file.write(total_message_hex.encode('utf-8') + b'\n')
+
+
+saving_method1()
+
