42e593c34318e79adc852d5fd6cd93b9959006f0
MauritsKuipers <74401262+MauritsKuipers@users.noreply.github.com>
2024-01-29 14:29:08 +0100

    there was aither erro related to opening file and port

--- recording/BINARY_store.py
+++ recording/BINARY_store.py
@@ -1,6 +1,6 @@
 import serial
 from datetime import datetime
-from pathlib import Patch
+from pathlib import Path
 import pynmea2
 
 
@@ -21,20 +21,22 @@ def saving_method1():
         file.write(hex_line.encode('utf-8') + b'\n')
 
 
-def saving_method(port, file_path):
-    with open(file_path, 'ab', buffering=0) as file:
-        try:
-            while True:
-                start_message = port.read(4)
-                payload_length_hex = start_message[-2:].hex()
-                payload_length_decimal = int(payload_length_hex, 16)
-                message_payload_and_end = port.read(payload_length_decimal + 3)
-                total_message = start_message + message_payload_and_end
-                total_message_hex = total_message.hex()
-                file.write(total_message_hex.encode('utf-8') + b'\n')
-                file.flush()  # Ensure data is immediately written to the file
-        except Exception as e:
-            print(f"An error occurred: {e}")
+def saving_method():
+    try:
+        while True:
+            start_message = port.read(4)
+            payload_length_hex = start_message[-2:].hex()
+            payload_length_decimal = int(payload_length_hex, 16)
+            message_payload_and_end = port.read(payload_length_decimal + 3)
+            total_message = start_message + message_payload_and_end
+            total_message_hex = total_message.hex()
+            file.write(total_message_hex.encode('utf-8') + b'\n')
+            file.flush()  # Ensure data is immediately written to the file
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        file.close()
 
 
 saving_method1()
