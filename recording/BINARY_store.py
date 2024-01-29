385fb16ae0cc2631b86ad63963d0cb68e35c575d
MauritsKuipers <74401262+MauritsKuipers@users.noreply.github.com>
2024-01-29 13:52:09 +0100

    updated saving mode

--- recording/BINARY_store.py
+++ recording/BINARY_store.py
@@ -21,15 +21,20 @@ def saving_method1():
         file.write(hex_line.encode('utf-8') + b'\n')
 
 
-def saving_method():
-    while True:
-        start_message = port.read(4)
-        payload_length_hex = start_message[-2:].hex()
-        payload_length_decimal = int(payload_length_hex, 16)
-        message_payload_and_end = port.read(payload_length_decimal + 3)
-        total_message = str(start_message) + str(message_payload_and_end)
-        total_message_hex = total_message.hex()
-        file.write(total_message_hex.encode('utf-8') + b'\n')
+def saving_method(port, file_path):
+    with open(file_path, 'ab', buffering=0) as file:
+        try:
+            while True:
+                start_message = port.read(4)
+                payload_length_hex = start_message[-2:].hex()
+                payload_length_decimal = int(payload_length_hex, 16)
+                message_payload_and_end = port.read(payload_length_decimal + 3)
+                total_message = start_message + message_payload_and_end
+                total_message_hex = total_message.hex()
+                file.write(total_message_hex.encode('utf-8') + b'\n')
+                file.flush()  # Ensure data is immediately written to the file
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 saving_method1()
