da751cf9ec2ae29b1e2d3c57dba4622452835ef5
mattiastenvoorde <80331307+mattiastenvoorde@users.noreply.github.com>
2024-01-31 14:49:27 +0100

    Update Binary_updated_processing.py

--- processing/Binary_updated_processing.py
+++ processing/Binary_updated_processing.py
@@ -1,59 +1,62 @@
 from datetime import datetime, timedelta
 import pytz
 import re
+import os
 
-file_path = 'processed_parsed_BINARYLog_2024_01_28_21_11_GEOS_Default_Settings_Repaired.txt'
-output_file_path = 'processed_parsed_BINARYLog_2024_01_28_21_11_GEOS_Default_Settings_Repaired_Post.txt'
+input_directory = 'binary_parsing/output_data'
+output_directory = 'processing'
 
-with open(file_path, 'r') as file:
-    lines = [line.strip() for line in file]
+# Ensure the output directory exists
+os.makedirs(output_directory, exist_ok=True)
 
-result_string = '\n'.join(lines)
-#print(result_string)
+def process_file(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        lines = [line.strip() for line in file]
 
-split_strings = result_string.split("{\"status\":")
+    result_string = '\n'.join(lines)
 
-if split_strings:
-    split_strings.pop(0)
+    split_strings = result_string.split("{\"status\":")
 
-final_strings = [sub_string.split('\"') for sub_string in split_strings]
+    if split_strings:
+        split_strings.pop(0)
 
-initial_time = float(re.sub(r"[^0-9.]", "", final_strings[0][21]))
-start_time = initial_time
+    final_strings = [sub_string.split('\"') for sub_string in split_strings]
 
-def gps_datetime(time_week, time_ms, leap_seconds=18):
-    gps_epoch = datetime(1980, 1, 6, tzinfo=pytz.utc)
-    return gps_epoch + timedelta(weeks=time_week, 
-                                 milliseconds=time_ms, 
-                                 seconds=-leap_seconds)
+    initial_time = float(re.sub(r"[^0-9.]", "", final_strings[0][21]))
+    start_time = initial_time
 
-# Open the text file for writing
-with open(output_file_path, 'w') as txtfile:
-    for i in range(len(final_strings)):
-        if final_strings[i][1] == 'complete':
-            #gps_week = (re.sub(r"[^0-9.]", "", final_strings[i][17]))
-            gps_sec = (re.sub(r"[^0-9.]", "", final_strings[i][21]))
-            #if not gps_week:
-            #    time = 'N/A'
-            #elif not gps_sec:
-            #    time = 'N/A'
-            #else:
-            time = (float(gps_sec)-float(final_strings[416][21]))/100+416
-            x = float((re.sub(r"[^0-9.-]", "", final_strings[i][60])))/100
-            y = float((re.sub(r"[^0-9.-]", "", final_strings[i][62])))/100
-            z = float((re.sub(r"[^0-9.-]", "", final_strings[i][64])))/100
-            vx = float((re.sub(r"[^0-9.-]", "", final_strings[i][66])))/100
-            vy = float((re.sub(r"[^0-9.-]", "", final_strings[i][68])))/100
-            vz = float((re.sub(r"[^0-9.-]", "", final_strings[i][70])))/100
-        else:
-            time = 'incomplete'
-            x = 'incomplete'
-            y = 'incomplete'
-            z = 'incomplete'
-            vx = 'incomplete'
-            vy = 'incomplete'
-            vz = 'incomplete'
-        # Write the data to the text file without the header
-        txtfile.write(f"{time}, {x}, {y}, {z}, {vx}, {vy}, {vz}\n")
+    with open(output_file_path, 'w') as txtfile:
+        for i in range(len(final_strings)):
+            if final_strings[i][9]=='3D':
+                first_lock=i
+                break
 
-print(f"Data (without header) has been written to {output_file_path}")
+        for i in range(len(final_strings)):
+            if final_strings[i][1] == 'complete' and final_strings[i][9] == '3D':
+                gps_sec = (re.sub(r"[^0-9.]", "", final_strings[i][21]))
+                time = (float(gps_sec) - 51840000) / 100
+                x = float((re.sub(r"[^0-9.-]", "", final_strings[i][60]))) / 100
+                y = float((re.sub(r"[^0-9.-]", "", final_strings[i][62]))) / 100
+                z = float((re.sub(r"[^0-9.-]", "", final_strings[i][64]))) / 100
+                vx = float((re.sub(r"[^0-9.-]", "", final_strings[i][66]))) / 100
+                vy = float((re.sub(r"[^0-9.-]", "", final_strings[i][68]))) / 100
+                vz = float((re.sub(r"[^0-9.-]", "", final_strings[i][70]))) / 100
+
+                # Write the data to the text file without the header
+                txtfile.write(f"{time}, {x}, {y}, {z}, {vx}, {vy}, {vz}\n")
+
+    print(f"Data (without header) has been written to {output_file_path}")
+
+# List all files in the input directory
+files = os.listdir(input_directory)
+
+for file in files:
+    # Check if the file is a text file (you can adjust the condition based on your file types)
+    if file.endswith('.txt'):
+        input_file_path = os.path.join(input_directory, file)
+        output_file_path = os.path.join(output_directory, f'{os.path.splitext(file)[0]}_post.txt')
+
+        # Process the file and save the result with the "_post" suffix
+        process_file(input_file_path, output_file_path)
+
+print("Processing completed for all files.")
